f2164b589099ca2b0cf812eb6c278246716a51cb
Wiktor Zdziechowski <wiktor.zdziechowski@exence.com>
2019-08-25 14:12:38 +0200

    [PAYEYE-14] Adds EditorActivity layout

--- app/src/main/java/com/example/payeyetask/ui/editor/EditorActivity.kt
+++ app/src/main/java/com/example/payeyetask/ui/editor/EditorActivity.kt
@@ -1,12 +1,30 @@
 package com.example.payeyetask.ui.editor
 
+import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.payeyetask.R
 import com.example.payeyetask.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_editor.*
 import org.koin.android.ext.android.inject
 
 class EditorActivity : BaseActivity<EditorActivityViewModel>(){
     override val layoutId = R.layout.activity_editor
     override val viewModel: EditorActivityViewModel by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initSpinner()
+    }
+
+    private fun initSpinner() = ArrayAdapter.createFromResource(
+        this,
+        R.array.gender_array,
+        android.R.layout.simple_spinner_item
+    ).also { adapter ->
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner_gender.adapter = adapter
+    }
 
     override fun bindViewHolder() {
 
